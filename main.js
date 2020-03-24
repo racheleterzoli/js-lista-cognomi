2abaef0d4a3cfdad322b5f1e061d7a296e7a000d
rachele <rachele.terzoli@gmail.com>
2020-03-24 16:55:54 +0100

    esercizio finito

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-
-var inserisci;
+var check;
+var inserisci, itemN, i, contenutoPrecedente;
 inserisci = prompt ("inserisci il tuo cognome e lo inserirò in oridne alfabetico. Premi ok per continuare");
 var cognomi = [inserisci , 'rossi', 'fattori', 'belli', 'baiocchi', 'uncini','bianchi'];
 cognomi.sort();
 document.getElementById('password').innerHTML = cognomi.sort();
+
+
+
+for (var i = 0; i < cognomi.length; i++){
+
+
+  if (cognomi[i] == inserisci) {
+    document.getElementById('password').innerHTML = inserisci + " è al " + (i+1) + " posto nella seguente lista:";
+  }
+
+}
+for (var i = 0; i < cognomi.length; i++){
+  itemN = cognomi[i];
+  contenutoPrecedente = document.getElementById('password').innerHTML;
+  document.getElementById('password').innerHTML = contenutoPrecedente + "<li>" + itemN + "</li>"
+
+}
